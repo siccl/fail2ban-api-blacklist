1934ddc2222efdf6d1c2e6b249917b4d9b9c40d3
Hansel Stange Gaete <hansel.stange@sic.cl>
2023-07-02 14:31:47 -0400

    tested

---
+++ README.md
@@ -0,0 +1,36 @@
+# Python Scripts for fail2Ban
+
+Another utilities project for fail2Ban
+github.com/siccl
+
+## Scripts
+
+### fail2ban-blacklist
+
+Save Banned IP into file and send to an API with JWT
+
+### fail2ban-telegram
+
+Send information to a telegram channel/chat/group using an existent bot
+
+## Installation
+
+* Copy Scripts files into /etc/fail2ban/scripts/
+* Copy Action files into /etc/fail2ban/action.d/
+* Copy o change Jail fails /etc/fail2ban/jail.d/
+
+## Usage
+
+Only need Jails are using that
+
+## Contributing
+
+for now you can take direct contact
+
+## Authors
+
+* [@siccl](https://github.com/siccl)
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- fail2ban-blacklist.py
+++ fail2ban-blacklist.py
@@ -1,31 +1,23 @@
 #!/usr/bin/python3
-"""
-Fail2Ban Blacklist
-Author: @siccl (github.com/siccl)
-Description: Save Banned IP into file and send to an API with JWT
-"""
 import sys
 import os
 import ipaddress
 import datetime
-import jwt
 import requests
+from dotenv import load_dotenv
 
-# Fail2Ban variables
-BAN_IP = sys.argv[1]
-BLACKLIST_FILE = os.environ['BLACKLIST_FILE']
+load_dotenv()
 
-# API variables
-API_URL = os.environ['API_URL']
-CONTENT_TYPE="Content-Type: application/json"
+# Set environment variables
+BLACKLIST_FILE = os.getenv('BLACKLIST_FILE')
 
 # Save banned IP into file function
-def save_banned_ip():
+def save_banned_ip(BAN_IP):
     with open(BLACKLIST_FILE, 'a') as file:
         file.write(BAN_IP + '\n')
 
 # Remove banned IP from file function
-def remove_banned_ip():
+def remove_banned_ip(BAN_IP):
     with open(BLACKLIST_FILE, 'r+') as file:
         lines = file.readlines()
         file.seek(0)
@@ -35,23 +27,24 @@ def remove_banned_ip():
         file.truncate()
 
 # Check if BAN_IP is valid function
-def is_valid_ip():
+def is_valid_ip(BAN_IP):
     # Check if IP is valid
+    TEST_IP = False
     try:
-        ipaddress.ip_address(BAN_IP)
+        TEST_IP = ipaddress.ip_address(BAN_IP)
         # Check if IP is local or private
         if ipaddress.ip_address(BAN_IP).is_private:
-            return False
+            TEST_IP = False
         if ipaddress.ip_address(BAN_IP).is_loopback:
-            return False
+            TEST_IP = False
         if ipaddress.ip_address(BAN_IP).is_link_local:
-            return False
+            TEST_IP = False
     except ValueError:
-        return False
-    return True
+        TEST_IP = False
+    return TEST_IP
 
 # Check if IP is banned function
-def is_banned_ip():
+def is_banned_ip(BAN_IP):
     # Check if IP is banned in file
     with open(BLACKLIST_FILE, 'r') as file:
         for line in file:
@@ -62,25 +55,29 @@ def is_banned_ip():
 
 # Generate JWT token function
 def generate_jwt_token():
+    import jwt
     # Generate JWT token
-    SECRET_KEY=os.environ['SECRET_KEY']
+    SECRET_KEY=os.getenv('JWT_SECRET_KEY')
+    ALGORITHM=os.getenv('JWT_ALGORITHM')
     NOW = datetime.datetime.utcnow()
-    EXP = NOW + datetime.timedelta(seconds=30)
+    EXP_TIME = int(os.getenv('JWT_EXP_TIME'))
+    EXP = NOW + datetime.timedelta(seconds=EXP_TIME)
     payload = {
-        "iss": "fail2ban-blacklist",
+        "iss": os.getenv('JWT_ISSUER'),
         "iat": NOW,
         "exp": EXP,
         "nbf": NOW,
     }
-    return jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
 
 # Send banned IP to API function
-def send_banned_ip():
+def send_banned_ip(BAN_IP):
+    API_URL = os.getenv('API_URL')
     headers = {
         'Authorization': 'Bearer ' + generate_jwt_token(),
         'Content-Type': 'application/json',
     }
-    data = '{"ip": "' + BAN_IP + '"}'
+    data = '{"IP": "' + BAN_IP + '"}'
     response = requests.post(API_URL, headers=headers, data=data)
     if response.status_code == 200:
         return True
@@ -94,25 +91,67 @@ def display_help():
     print("Usage: fail2ban-blacklist.py [ban|unban|check] [IP]")
 
 # check dependencies
-def check_dependencies():
+def check_dependencies(test=True):
+    global BLACKLIST_FILE
+    # Check if dependencies are installed
+    # Check run script as root or sudo
+    if not os.geteuid() == 0:
+        print("Error: Please run script as root or sudo")
+        sys.exit(1)
+    CHECK_ERROR = False
     # Check if dependencies are installed
     try:
         import jwt
         import requests
+        import ipaddress
+        import datetime
     except ImportError:
         print("Error: Please install dependencies")
         print("pip3 install -r requirements.txt")
+        CHECK_ERROR = True
+    if CHECK_ERROR:
         sys.exit(1)
+    else:
+        # Check if environment variables are set
+        # Validate folder BLACKLIST_FILE if exists
+        if not os.path.exists(os.path.dirname(BLACKLIST_FILE)):
+            print("Error: BLACKLIST_FILE folder does not exist")
+            CHECK_ERROR = True
+    if CHECK_ERROR:
+        sys.exit(1)
+    else:
+        if test:
+            print("Dependencies are installed and valid")
 
 # Main function
 def main():
+    # Check if arguments are passed
+    try:
+        sys.argv[1]
+        BAN_IP = sys.argv[2]
+    except IndexError:
+        display_help()
+        return False
     if len(sys.argv) == 1:
         display_help()
     elif sys.argv[1] == 'ban':
-        if is_valid_ip() and not is_banned_ip():
-            save_banned_ip()
-            return send_banned_ip()
+        check_dependencies(False)
+        if is_valid_ip(BAN_IP) and not is_banned_ip(BAN_IP):
+            save_banned_ip(BAN_IP)
+            return send_banned_ip(BAN_IP)
+        else:
+            if not is_valid_ip(BAN_IP):
+                print("Error: " + BAN_IP + "is not valid")
+            elif is_banned_ip(BAN_IP):
+                print("Error: IP is already banned")
+            return False
     elif sys.argv[1] == 'unban':
-        remove_banned_ip()
+        check_dependencies(False)
+        remove_banned_ip(BAN_IP)
+    elif sys.argv[1] == 'check':
+        check_dependencies()
     else:
         display_help()
+# Run main function
+if __name__ == "__main__":
+    main()
